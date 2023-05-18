6c34b8f07714358f2f1f0ebecd251819203e157a
Pola Stróżak <pstrozak@edu.cdv.pl>
2023-05-18 05:36:00 +0200

    Add "gameplay" scene and simple UI

--- Assets/UI/Main_Menu_Script.cs
+++ Assets/UI/Main_Menu_Script.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Rendering;
 
 public class Main_Menu_Script : MonoBehaviour
 {
-        [Header("Levels_To_Load")]
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject comfirmationPrompt = null;
+
+
+    [Header("Levels_To_Load")]
     public string _newGameLevel;
     private string levelToLoad;
 
@@ -27,5 +34,23 @@ public class Main_Menu_Script : MonoBehaviour
     public void QuitGame ()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        comfirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        comfirmationPrompt.SetActive(false);
     }
 }
